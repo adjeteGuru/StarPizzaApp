38ac7c4bc01730c8976f52d15f6ce2373e4a7714
adjeteGuru <justinadjev@hotmail.com>
2020-10-29 10:29:02 +0000

    feat: some controllers and views implemented

---
+++ StarPizzaShop/Controllers/TelephoneController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using StarPizzaShop.Database;
+using StarPizzaShop.Models;
+
+namespace StarPizzaShop.Controllers
+{
+    public class TelephoneController : Controller
+    {
+        private readonly StarPizzaContext _context;
+
+        public TelephoneController(StarPizzaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Telephone
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.PhoneNumbers.ToListAsync());
+        }
+
+        // GET: Telephone/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telephone = await _context.PhoneNumbers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (telephone == null)
+            {
+                return NotFound();
+            }
+
+            return View(telephone);
+        }
+
+        // GET: Telephone/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Telephone/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,PhoneNumber")] Telephone telephone)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(telephone);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(telephone);
+        }
+
+        // GET: Telephone/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telephone = await _context.PhoneNumbers.FindAsync(id);
+            if (telephone == null)
+            {
+                return NotFound();
+            }
+            return View(telephone);
+        }
+
+        // POST: Telephone/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,PhoneNumber")] Telephone telephone)
+        {
+            if (id != telephone.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(telephone);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TelephoneExists(telephone.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(telephone);
+        }
+
+        // GET: Telephone/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var telephone = await _context.PhoneNumbers
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (telephone == null)
+            {
+                return NotFound();
+            }
+
+            return View(telephone);
+        }
+
+        // POST: Telephone/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var telephone = await _context.PhoneNumbers.FindAsync(id);
+            _context.PhoneNumbers.Remove(telephone);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TelephoneExists(int id)
+        {
+            return _context.PhoneNumbers.Any(e => e.Id == id);
+        }
+    }
+}

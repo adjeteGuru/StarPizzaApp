e159637a49c2cc950eff746aaeeac6ca37acb351
adjeteGuru <justinadjev@hotmail.com>
2020-10-29 11:28:02 +0000

    feat: more controllers and view(index) implemented

---
+++ StarPizzaShop/Controllers/DrinkController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarPizzaShop.Database;
+
+namespace StarPizzaShop.Controllers
+{
+    public class DrinkController : Controller
+    {
+        private readonly StarPizzaContext _context;
+        public DrinkController(StarPizzaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.Drinks.ToList());
+        }
+    }
+}
---
+++ StarPizzaShop/Controllers/PackIncController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using StarPizzaShop.Database;
+
+namespace StarPizzaShop.Controllers
+{
+    public class PackIncController : Controller
+    {
+        private readonly StarPizzaContext _context;
+        public PackIncController(StarPizzaContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            return View(_context.PackIncs.ToList());
+        }
+    }
+}
